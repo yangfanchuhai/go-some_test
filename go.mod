48fda15fed47659d053703a2c81a2e5707a92122
lijin05 <lijin05@fenbi.com>
2022-04-18 15:36:09 +0800

    ADD: go mod test

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/yangfanchuhai/go-some_test
+
+go 1.16
+
+require github.com/sirupsen/logrus v1.8.0
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	logrus.Println("第一个mod项目测试")
+}
